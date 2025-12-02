8bf03bfc02f862df1b19e82fde52159edd1fc8a6
Umar Khan <umarkhaneducation@gmail.com>
2025-12-02 20:34:09 +0500

    Active Button

--- client-dashboard/components/ui/Filter.jsx
+++ client-dashboard/components/ui/Filter.jsx
@@ -9,6 +9,8 @@ const Filter = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
 
+  const activeFilter = searchParams.get("discount") || "all";
+
   function handleClick(value) {
     const params = new URLSearchParams(searchParams.toString());
 
@@ -23,13 +25,24 @@ const Filter = () => {
 
   return (
     <ButtonGroup>
-      <Button variant="outline" onClick={() => handleClick("all")}>
+      <Button
+        variant={activeFilter === "all" ? "default" : "outline"}
+        onClick={() => handleClick("all")}
+      >
         All
       </Button>
-      <Button variant="outline" onClick={() => handleClick("no-discount")}>
+
+      <Button
+        variant={activeFilter === "no-discount" ? "default" : "outline"}
+        onClick={() => handleClick("no-discount")}
+      >
         No Discount
       </Button>
-      <Button variant="outline" onClick={() => handleClick("with-discount")}>
+
+      <Button
+        variant={activeFilter === "with-discount" ? "default" : "outline"}
+        onClick={() => handleClick("with-discount")}
+      >
         With Discount
       </Button>
     </ButtonGroup>
